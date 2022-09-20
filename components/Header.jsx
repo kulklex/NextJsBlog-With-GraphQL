6ecd86b08f253b01649bd0ab7b5fe8659cb54df7
kulklex <kulklex@gmail.com>
2022-09-20 12:10:21 +0100

    header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,16 @@
-import React, {useContext} from 'react'
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
+import { getCategories } from '../services';
 
-const categories = [
-    {name: 'React', slug: 'react'},
-    {name: 'Next', slug: 'nextJS'}
-]
 
 function Header() {
+const [categories, setCategories] = useState([]);
+
+useEffect(() => {
+    getCategories()
+    .then((cat) => setCategories(cat))
+}, [])
+
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className="border-b w-full inline-block border-blue-400 py-8">
@@ -17,9 +21,9 @@ function Header() {
                     </span>
                 </Link>
             </div>
-            <div className="hidden md:float-left md:contents ">
+            <div className="overflow-hidden md:float-left md:contents ">
                 {categories.map((cat) => (<Link key={cat.slug} href={`/category/${cat.slug}`}>
-                    <span className="mt-2 md:float-right align-middle ml-4 font-semibold cursor-pointer">{cat.name}</span>
+                    <span className="mt-2 md:float-right align-middle pr-4 font-semibold cursor-pointer">{cat.name}</span>
                 </Link>))}    
             </div>     
         </div>
