fc789dd2a5e5d579aa904ca4874d96853a0ff3d9
kulklex <kulklex@gmail.com>
2022-09-20 12:07:17 +0100

    categories component

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import Link from 'next/link';
+import React, {useState, useEffect} from 'react'
+import { getCategories } from '../services';
 
 export default function Categories() {
+ const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories()
+    .then((newCat) => setCategories(newCat))
+  }, [])
+
   return (
-    <div>Categories</div>
+    <div className='bg-white shadow-xl rounded-lg p-8 mb-8'>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Categories
+      </h3>
+      {categories.map((cat) => (
+        <Link key={cat?.slug} href={`/category/${cat?.slug}`}>
+          <span className="cursor-pointer mb-3 pb-3 block">
+            {cat?.name}
+          </span>
+        </Link>
+      ))}
+    </div>
   )
 }
