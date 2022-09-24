d5e6a3a9f19fe317280fb729bccdccfd1536451a
kulklex <kulklex@gmail.com>
2022-09-24 09:33:32 +0100

    Added loader component

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -6,9 +6,16 @@ import CommentsForm from '../../components/CommentsForm'
 import PostDetail from '../../components/PostDetail'
 import PostWidget from '../../components/PostWidget'
 import { getPostDetails, getPosts } from '../../services'
+import { useRouter } from 'next/router'
+import { Loader } from '../../components/Loader'
 
 export default function PostDetails({post}) {
-  return (
+    const router = useRouter();
+    if (router.isFallback) {
+        return <Loader/>
+    }
+    
+    return (
     <div className='container mx-auto px-10 mb-8'>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
             <div className="col-span-1 lg:col-span-8">
