b8d1414d16a4731367abfacfd8ee70b10fb68c0c
kulklex <kulklex@gmail.com>
2023-02-12 19:57:40 +0100

    Bug fixes

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -13,13 +13,13 @@ export const FeaturedPostCard = ({ post }) => (
       <div className="flex items-center absolute bottom-5 w-full justify-center">
         <Image
           unoptimized
-          alt={post?.author.name}
+          alt={post?.author?.name}
           height="30px"
           width="30px"
           className="align-middle drop-shadow-lg rounded-full"
-          src={post?.author.photo.url}
+          src={post?.author?.photo.url}
         />
-        <p className="inline align-middle text-white text-shadow ml-2 font-medium">{post.author.name}</p>
+        <p className="inline align-middle text-white text-shadow ml-2 font-medium">{post?.author?.name}</p>
       </div>
     </div>
     <Link href={`/posts/${post?.slug}`}><span className="cursor-pointer absolute w-full h-full" /></Link>
